e6696523e9f5e87e6c64f9cd19275cdf157a3834
Atif Patel <atifp89@gmail.com>
2024-04-25 17:06:07 +0100

    mk1 updated

--- simple_leader_MK1.py
+++ simple_leader_MK1.py
@@ -1,39 +1,70 @@
-import numpy as np
-import random
-from scipy.optimize import minimize_scalar, minimize
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_percentage_error
+"""
+A simple leader for the Stackelberg game.
+"""
 from base_leader import Leader
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
-__author__ = "Rokas"
-
-class SimpleLeader(Leader):
+class MK1SimpleLeader(Leader):
+    data_set_X = np.array([])
+    data_set_Y = np.array([])
+    reg_poly = None
+    reg_linear = None
+    date = 100
 
     def __init__(self, name):
-        self.data_size = 100
-        self.model_weight = np.array([])
-        self.leader_data = np.array([])
-        self.follower_data = np.array([])
-        self.reg = LinearRegression()
-        # If you want to initialize something here, do it before the super() call.
+        self.reg_poly = PolynomialFeatures(degree=2)
+        self.reg_linear = LinearRegression()
+        self.data_set_X = np.array([])
+        self.data_set_Y = np.array([])
         super().__init__(name)
 
+    def new_price(self, date: int) -> float:
+        self.log(date)
+        self.update_model(date)
 
-    def load_historical_data(self):
-        self.leader_data = np.array([self.get_price_from_date(i)[0] for i in range(1, 101)])
-        self.follower_data = np.array([self.get_price_from_date(i)[1] for i in range(1, 101)])
+        # Fit linear regression
+        self.reg_linear.fit(self.data_set_X.reshape(-1, 1), self.data_set_Y)
+        pred_linear = self.reg_linear.predict(self.data_set_X[-1].reshape(-1, 1))
+        error_linear = mean_squared_error(self.data_set_Y, self.reg_linear.predict(self.data_set_X.reshape(-1, 1)))
+
+        # Transform features for polynomial regression
+        transformed_X_poly = self.reg_poly.fit_transform(self.data_set_X.reshape(-1, 1))
+        
+        # Fit polynomial regression
+        self.reg_linear.fit(transformed_X_poly, self.data_set_Y)
+        pred_poly = self.reg_linear.predict(transformed_X_poly[-1].reshape(1, -1))
+        error_poly = mean_squared_error(self.data_set_Y, self.reg_linear.predict(transformed_X_poly))
+
+        if error_poly < error_linear:
+            return float(pred_poly)
+        
+        return float(pred_linear)
+
 
     def start_simulation(self):
-        """
-        A function run at the beginning of the simulation.
-        """
         self.log("Start of simulation")
 
-        self.load_historical_data()
+        leader_prices = []
+        follower_prices = []
+        for i in range(1, 101):
+            leader, follower = self.get_price_from_date(i)
+            leader_prices.append(leader)
+            follower_prices.append(follower)
 
-        self.reg.fit(self.profit(self.leader_data, self.follower_data).reshape(-1, 1), self.follower_data)
+        self.data_set_X = np.array(leader_prices)
+        self.data_set_Y = np.array(follower_prices)
+
+        changed_X = self.profit(self.data_set_X, self.data_set_Y)
+        
+        # Transform features for polynomial regression
+        transformed_X_poly = self.reg_poly.fit_transform(changed_X.reshape(-1, 1))
+
+        # Fit polynomial regression
+        self.reg_linear.fit(transformed_X_poly, self.data_set_Y)
 
     def end_simulation(self):
         """
@@ -41,66 +72,16 @@ class SimpleLeader(Leader):
         """
         self.log("End of simulation")
 
+    def update_model(self, date):
+        yesterday = date - 1
+        yesterday_prices = self.get_price_from_date(yesterday)
+        self.data_set_X = np.append(self.data_set_X, yesterday_prices[0])
+        self.data_set_Y = np.append(self.data_set_Y, yesterday_prices[1])
+
     def profit(self, ul, uf):
-        return (2-ul+0.3*uf) * (ul - 1)
-
-    def update_prediction(self, date):
-        previous_data = self.get_price_from_date(date - 1)
-        previous_leader_data = previous_data[0]
-        previous_follower_data = previous_data[1]
-
-        self.leader_data = np.append(self.leader_data, previous_leader_data)
-        self.follower_data = np.append(self.follower_data, previous_follower_data)
-
-        sample_weight = np.array([self.window(i) for i in range(1, date+1)])
-
-        self.reg.fit(self.profit(self.leader_data, self.follower_data).reshape(-1, 1), self.follower_data, sample_weight=sample_weight)
-
-    def window(self, date):
-        res = minimize_scalar(lambda weight: self.window_o(weight,date))
-        return res.x
-
-    def window_o(self, weight, date):
-        window_size = 10
-        if date < 10:
-            window_size = date
-        weights = np.array([1, weight])
-        sum = 0
-        for i in range (window_size):
-            follower_in_window = np.array([self.follower_data[len(self.follower_data) - window_size - i]])
-            follower_in_window = np.insert(follower_in_window, 0, 1)
-            part1 = self.leader_data[np.size(self.leader_data)-window_size-i] - (np.matmul(follower_in_window, weights))
-            sum += part1**2
-        return sum
-
-    def new_price(self, date: int) -> float:
-        """
-        A function for setting the new price of each day.
-        :param date: date of the day to be updated
-        :return: (float) price for the day
-        """
-        self.log(date)
-
-        self.update_prediction(date)
-
-        A = self.reg.coef_[0]
-        B = self.reg.intercept_
-
-        def profit_ul(x):
-            part1 = (-A*x**2)+(3*A*x)-(2*A)+B
-            part2 = 1 - 0.3*A*x + 0.3*A
-            part3 = part1/part2
-            part4 = 2 - x + 0.3 * part3
-            return ((x - 1) * part4)
-
-        price = minimize_scalar(
-            lambda x: -profit_ul(x), method="Bounded", bounds=(1, 2)).x
-
-        self.date = date
-
-        return float(price)
+        return (2 - ul + 0.3 * uf) * (ul - 1)
 
 
 if __name__ == '__main__':
     # Make sure you set this to your group number!
-    SimpleLeader('12')
+    MK1SimpleLeader('12')
